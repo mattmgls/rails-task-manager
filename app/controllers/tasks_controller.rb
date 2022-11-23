1ad2e64f584d4db5629eb1f2f0550cd79a45c300
hellboy <matiascfh98@gmail.com>
2022-11-23 15:48:40 -0300

    Refactorization done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -22,11 +23,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to task_path
     else
@@ -35,7 +34,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -44,5 +42,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  delete "tasks/:id", to: "tasks#destroy"
-  patch "tasks/:id/", to: "tasks#update"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks", to: "tasks#index"
+
+  # delete "tasks/:id", to: "tasks#destroy"
+  # patch "tasks/:id/", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks", to: "tasks#index"
+
+  resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
