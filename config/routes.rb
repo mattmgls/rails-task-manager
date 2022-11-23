e014d4bde22ee8de9f7b46d1133fade89fd9e044
hellboy <matiascfh98@gmail.com>
2022-11-23 15:32:18 -0300

    Rails CRUD created successfully

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  delete "tasks/:id", to: "tasks#destroy"
+  patch "tasks/:id/", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
   get "tasks", to: "tasks#index"
 end
